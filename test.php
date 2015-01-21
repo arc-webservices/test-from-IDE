e7ce859efcd80c99881e71b313dde2465da99a34
arc-webservices <arc-webservices@mail.nasa.gov>
2015-01-21 12:30:21 -0800

    test

--- test.php
+++ test.php
@@ -1,8 +1,13 @@
 <?php
-$output = shell_exec('git pull');
+//$output = shell_exec('git pull');
 $output = shell_exec('git add . -u');
+var_dump($output);
 $output = shell_exec('git commit -m "test"');
+echo "\n..............................................\n";
+var_dump($output);
+echo "\n..............................................\n";
 $output = shell_exec('git push origin master');
+var_dump($output);
 
 echo "done ............";
 
